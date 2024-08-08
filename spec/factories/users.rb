6dfbc8dff0b3754280a701e616346f5f7f9628b4
David Lafontant <lafontant.david@gmail.com>
2024-08-08 15:42:10 -0400

    Fix linting errors with rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email {"email@email.com"}
-    password {'pasword123456789'}
-    role {0}
+    email { 'email@email.com' }
+    password { 'pasword123456789' }
+    role { 0 }
   end
 end
