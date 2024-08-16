2a0301a1eeb5abc0ca1c174fa48ed02dfe0b2bef
David Lafontant <lafontant.david@gmail.com>
2024-08-16 19:51:34 -0400

    Fix typo in create produtc migration

--- db/migrate/20240816234250_create_products.rb
+++ db/migrate/20240816234250_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[7.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.decimal :price, precision: 10, scale: 2 , default: 0.00, 
+      t.decimal :price, precision: 10, scale: 2 , default: 0.00
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
