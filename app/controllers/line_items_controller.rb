8c51c2615768e5d5183560adc64553196e87833c
David Lafontant <lafontant.david@gmail.com>
2024-08-17 16:05:50 -0400

    Remove notice when create line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,7 +23,6 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-
     product = Product.find(params[:product_id])
     # @line_item = @cart.line_items.build(product: product)
     # @line_item = LineItem.new(line_item_params)
@@ -32,8 +31,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         # format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
-        format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
-      
+        # format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
+        format.html { redirect_to cart_url(@line_item.cart) }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -47,8 +46,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         # format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
-        format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
-        
+        # format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
+        format.html { redirect_to cart_url(@line_item.cart) }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit, status: :unprocessable_entity }
