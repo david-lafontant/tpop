17fc320a1e0ec286f04c7ced6ff134763b926a0f
David Lafontant <lafontant.david@gmail.com>
2024-08-17 15:10:44 -0400

    Secure line_items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -25,8 +25,9 @@ class LineItemsController < ApplicationController
   def create
 
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    # @line_item = @cart.line_items.build(product: product)
     # @line_item = LineItem.new(line_item_params)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
@@ -74,6 +75,7 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id, :quantity)
+      params.require(:line_item).permit(:product_id)
+
     end
 end
